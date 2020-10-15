77ccccb75dbd4163f85628b1e8ce8c31ef114b4f
deloiter <mybloomwork@gmail.com>
2020-10-15 22:50:00 +0100

    deloiters app, testpage

---
+++ apps/core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+from apps.store.models import Product, Category
+
+
+def frontpage(request):
+    products = Product.objects.filter(is_featured=True)
+    featured_categories = Category.objects.filter(is_featured=True)
+
+
+    context = {
+        'products': products,
+        'featured_categories': featured_categories
+
+    }
+
+    return render(request, 'frontpage.html', context)
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
