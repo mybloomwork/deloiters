52026ae1a9baa452366cb31d91cff2b5af7b5f15
mybloomwork <68631267+mybloomwork@users.noreply.github.com>
2020-10-16 20:35:12 +0100

    Update settings.py

--- bloom/settings.py
+++ bloom/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dynarielinnovations@gmail.com'
-EMAIL_HOST_PASSWORD = 'jesusistheking271982'
+EMAIL_HOST_PASSWORD = 'oluwadamilola1950'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'cart'
